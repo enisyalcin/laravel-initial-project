dca7f5ee82908aa26077e6c1634767a16ecd591c
Enis Yalçın <enisyalcin96@hotmail.com>
2022-07-04 18:50:12 +0300

    Update books columns

--- database/migrations/2022_07_04_143341_create_books_table.php
+++ database/migrations/2022_07_04_143341_create_books_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('author');
-            $table->string('photo_path');
+            $table->string('photo')->nullable();
             $table->string('isbn');
             $table->timestamps();
         });
